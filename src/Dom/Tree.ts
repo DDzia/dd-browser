70ad9b55512e41e67a8f25275fd7363e16631632
Dzianis Dziarkach <Dzianis_Dziarkach@epam.com>
2018-03-04 00:36:35 +0300

    added summary to Tree.isChild()

--- src/Dom/Tree.ts
+++ src/Dom/Tree.ts
@@ -1,4 +1,12 @@
 export class Tree {
+  /**
+   * Detect if some HTML element is child on any level.
+   * @static
+   * @param {HTMLElement} root Parent HTMLElement.
+   * @param {HTMLElement} toCheck HTMLElement to check.
+   * @returns is child or not boolean value.
+   * @memberof Tree
+   */
   public static isChild(root: HTMLElement, toCheck: HTMLElement) {
     let cursorElement: HTMLElement | null = toCheck;
     while (cursorElement !== document.body) {
