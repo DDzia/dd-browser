7ab9e7d02fef303cedbd1db3f65e2a08f5d60087
Dzianis Dziarkach <Dzianis_Dziarkach@epam.com>
2018-06-10 02:21:12 +0300

    added browser detection feature

--- karma.conf.js
+++ karma.conf.js
@@ -1,30 +1,78 @@
-module.exports = (config) => {
+// Karma configuration
+// Generated on Sun Jun 10 2018 00:59:10 GMT+0300 (Belarus Standard Time)
+
+module.exports = function(config) {
   config.set({
-    singleRun: true,
-    frameworks: ["mocha", "chai", "karma-typescript"],
-    browsers: [
-      "PhantomJS"
-    ],
-    basePath: "",
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha', 'chai', 'karma-typescript'],
+
+
+    // list of files / patterns to load in the browser
     files: [
       'src/**/*.ts'
     ],
-    logLevel: config.LOG_INFO,
-    plugins: [
-      "karma-*"
+
+
+    // list of files / patterns to exclude
+    exclude: [
     ],
-    reporters: ["mocha"],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      "src/**/*.ts": "karma-typescript"
+      '**/*.ts': ['karma-typescript']
     },
-    typescriptPreprocessor: {
-      options: {
-        sourceMap: true, // generate source maps
-        noResolve: false // enforce type resolution
-      },
-      transformPath: function(path) {
-        return path.replace(/\.ts$/, '.js');
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['mocha'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: [
+      'ChromeDebugging',
+    ],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+    customLaunchers: {
+      ChromeDebugging: {
+        base: 'Chrome',
+        flags: [ '--remote-debugging-port=9333' ]
       }
     },
-  });
+  })
 }
